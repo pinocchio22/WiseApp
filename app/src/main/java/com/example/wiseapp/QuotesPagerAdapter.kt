98482ecc725e65354c95a5a93695f302f9d1de4a
pinocchio22 <vviian.2@gmail.com>
2021-08-03 21:13:33 +0900

    feat : QuotesPagerAdapter
    onCreateViewHolder 에서는 ViewType 형태의 아이템뷰를 위한 뷰홀더 객체 생성
    onBindViewHolder 에서는 position에 해당하는 데이터를 뷰홀더의 아이템에 표시

---
+++ app/src/main/java/com/example/wiseapp/QuotesPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.wiseapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-08-03
+ * @desc
+ */
+
+class QuotesPagerAdapter : RecyclerView.Adapter<QuotesPagerAdapter.QuotesViewHolder>() {
+
+    class QuotesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuotesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: QuotesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
